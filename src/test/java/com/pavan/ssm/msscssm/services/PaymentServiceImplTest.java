624c1419accf150f61403dd100c1492bcd82aea3
PAVAN-KUMAR23 <chpavankumarreddy1234@gmail.com>
2022-09-23 15:01:00 +0530

    preAuth to Auth

--- src/test/java/com/pavan/ssm/msscssm/services/PaymentServiceImplTest.java
+++ src/test/java/com/pavan/ssm/msscssm/services/PaymentServiceImplTest.java
@@ -5,6 +5,7 @@ import com.pavan.ssm.msscssm.domain.PaymentEvent;
 import com.pavan.ssm.msscssm.domain.PaymentState;
 import com.pavan.ssm.msscssm.repository.PaymentRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -45,6 +46,24 @@ class PaymentServiceImplTest {
         System.out.println(sm.getState().getId());
 
         System.out.println(preAuthedPayment);
+
+    }
+
+    @Transactional()
+    @RepeatedTest(10)
+    void testAuth(){
+        Payment savedPayment = paymentService.newPayment(payment);
+        System.out.println(savedPayment.getState());
+        StateMachine<PaymentState, PaymentEvent> preAuthSM = paymentService.preAuth(savedPayment.getId());
+        if (preAuthSM.getState().getId() == PaymentState.PRE_AUTH) {
+            System.out.println("Payment is Pre Authorized");
+
+            StateMachine<PaymentState, PaymentEvent> authSM = paymentService.authorizePayment(savedPayment.getId());
+
+            System.out.println("Result of Auth: " + authSM.getState().getId());
+        } else {
+            System.out.println("Payment failed pre-auth...");
+        }
 
     }
 }
